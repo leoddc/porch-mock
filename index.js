41a7a62a50f150f8738fb7ff1b8bae81f52470ad
Leo Davidson <leovidson@gmail.com>
2024-02-21 14:52:13 -0500

    initial endpoint

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const port = 3000;
+const numberOfFakes = 100;
 const app = express();
+const errorChance = 0.0; // chance that endpoint will throw an error
+const { faker } = require('@faker-js/faker');
 
-app.get('/', (req, res) => {
-    console.log(req);
+function createRandomRow() {
+    return {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        donatingFood: faker.datatype.boolean(),
+        donatingMoney: faker.datatype.boolean(),
+        location: {
+            lat: faker.location.latitude,
+            lon: faker.location.longitude
+        },
+    };
+}
+
+app.get('/donor-list', (req, res) => {
+    let donorList = [];
+    let i = 0;
+    do {
+        donorList.push(createRandomRow());
+    }
+    while (i < numberOfFakes);
+
+    if (Math.random() > errorChance || req.query.error) {
+        return res.status(400).send('Error getting donor list.');
+    }
+
+    return res.send(donorList);
 });
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+    console.log(`Mock endpoints running locally. Use http://localhost:${port}/...`);
 });
